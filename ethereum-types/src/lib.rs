fa7a5e65d06aaec7e2112ad769b84dff26362bd9
brockelmore <31553173+brockelmore@users.noreply.github.com>
2020-01-01 19:59:57 -0500

    export u96 and u32

--- ethereum-types/src/lib.rs
+++ ethereum-types/src/lib.rs
@@ -4,8 +4,8 @@ mod hash;
 mod uint;
 
 pub use ethbloom::{Bloom, BloomRef, Input as BloomInput};
-pub use hash::{BigEndianHash, H128, H160, H256, H264, H32, H512, H520, H64};
-pub use uint::{FromDecStrErr, U128, U256, U512, U64};
+pub use hash::{BigEndianHash, H64, H128, H160, H256, H264, H32, H512, H520};
+pub use uint::{FromDecStrErr, U32, U64, U96, U128, U256, U512};
 
 pub type Address = H160;
 pub type Secret = H256;
